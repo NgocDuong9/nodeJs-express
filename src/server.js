489172fa7a65f62a7de4ffc88c01e782469a606b
NguyenNgocDuong <78304672+NguyenNgocDuongHumg@users.noreply.github.com>
2024-03-07 14:14:37 +0700

    connection database

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const configEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express(); // app express
 const port = process.env.PORT || 8080; // port
@@ -12,6 +13,20 @@ configEngine(app);
 
 // khai bao route
 app.use("/", webRoutes);
+
+//test connection
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  port: 3307,
+  user: "root",
+  password: "123456",
+  database: "nodeJs",
+});
+
+connection.query("SELECT * FROM Users;", function (err, results, fields) {
+  console.log("result ===>", results); // results contains rows returned by server
+  console.log("fields", fields); // fields contains extra meta data about results, if available
+});
 
 app.listen(port, host, () => {
   console.log(`Example app listening on port ${port}`);
